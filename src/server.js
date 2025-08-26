12e968fc1b1008ffc8c8d842640d3ddc34c4ff16
papinwit-l <pae.papinwit@gmail.com>
2025-08-26 10:37:55 +0700

    edit port

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ app.use("/data-sheets", dataSheetsRouter);
 
 app.use(errHandler);
 
-const PORT = process.env.PORT || 8100;
+const PORT = process.env.PORT || 8200;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
